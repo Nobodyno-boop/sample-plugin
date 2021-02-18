429f10e8879c02b98eeb22f37c8369bc4efe28fb
Bloub_ <poulpi.hack@gmail.com>
2021-02-18 17:59:07 +0100

    first push

---
+++ src/main/java/fr/nobody/sample/Sample.java
@@ -0,0 +1,20 @@
+package fr.nobody.sample;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Sample extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
